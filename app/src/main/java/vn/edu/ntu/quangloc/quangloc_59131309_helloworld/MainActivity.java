eb91fb6cd167d0e9be159c5f45c24910314d0854
dauquangloc05 <loc.dq.59cntt@ntu.edu.vn>
2020-04-10 10:58:46 +0700

    V 1.0

---
+++ app/src/main/java/vn/edu/ntu/quangloc/quangloc_59131309_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.quangloc.quangloc_59131309_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
